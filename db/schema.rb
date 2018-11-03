01427c2ed900c4a8e106c209de7ae7475514aad7
github username <github email address>
2018-11-03 15:37:31 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20151230232412) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.integer  "age"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
